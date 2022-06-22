80e5c24b542f794713b8edb72fa162d8837bd9ec
Guilherme <guilima102013@gmail.com>
2022-06-22 00:00:51 -0300

    Fazendo header

---
+++ galeria.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clorocine</title>
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</head>
+
+<body>
+<nav class="nav-extended purple lighten-3">
+    <div class="nav-wrapper">
+
+      <div class="nav-header center">
+        <h1 class="brand-logo">Cloricine</h1>  
+      </div>
+      
+      
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="cadastrar.php">Cadastrar</a></li>        
+      </ul>
+    </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent purple darken-1">
+        <li class="tab"><a class="active" href="#test1">Todos</a></li>
+        <li class="tab"><a href="#test2">Assistidos</a></li>
+        <li class="tab"><a href="#test3">Favoritos</a></li>        
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">JavaScript</a></li>
+  </ul>
+
+  <div id="test1" class="col s12">Test 1</div>
+  <div id="test2" class="col s12">Test 2</div>
+  <div id="test3" class="col s12">Test 3</div>
+  <div id="test4" class="col s12">Test 4</div>
+        
+
+</body>
+
+</html>
