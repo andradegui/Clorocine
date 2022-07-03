e3064dedc5017e27a4680a886685c1bea405e299
Guilherme <guilima102013@gmail.com>
2022-07-02 21:47:38 -0300

    Fazendo tela cadastro, criando header

---
+++ cadastrar.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clorocine</title>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</head>
+
+<body>
+    <nav class="nav-extended purple lighten-3">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>CLOROCINE</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent purple darken-1">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="row">
+        <div class="col s6 offset-s3">
+            <div class="card">
+                <div class="card-content white-text">
+                    <span class="card-title">Cadastrar Filme</span>
+                    <!-- Titulo -->
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="titulo" type="text" class="validate" require>
+                            <label for="titulo">Título do filme</label>
+                        </div>
+                    </div>
+                    <!-- Sinopse -->
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <textarea id="sinopse" class="materialize-textarea"></textarea>
+                                    <label for="sinopse">Sinopse</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- nota -->
+                    <div class="row">
+                        <div class="input-field col s4">
+                            <input id="nota" type="number" step=".1" min=0 max=10 class="validate" require>
+                            <label for="nota">Nota</label>
+                        </div>
+                    </div>
+                    <!-- Capa -->
+                    <div class="file-field input-field">
+                        <div class="btn purple lighten-2">
+                            <span>Capa</span>
+                            <input type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+                </div>
+                <!-- Botões -->
+                <div class="card-action">
+                    <a class="btn waves-effect waves-light grey" href="galeria.php">Cancelar</a>
+                    <a class="btn waves-effect waves-light purple" href="#">Confirmar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
