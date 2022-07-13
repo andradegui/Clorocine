932cf7c673f2d68b03991e96fedbac0eb842a42b
Guilherme <guilima102013@gmail.com>
2022-07-13 17:56:46 -0300

    Trabalhando com PHP, deixando catalogo de filmes estatico

--- galeria.php
+++ galeria.php
@@ -9,6 +9,22 @@ $filme1 = [
   "poster" => "https://www.themoviedb.org/t/p/original/u49fzmIJHkb1H4oGFTXtBGgaUS1.jpg"
 ];
 
+$filme2 = [
+  "titulo" => "Thor: Love And Thunder",
+  "nota" => 10,
+  "sinopse" => "Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Mé faiz elementum girarzis, nisi eros vermeio. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Nec orci ornare consequat.",
+  "poster" => "https://www.themoviedb.org/t/p/original/6OEBp0Gqv6DsOgi8diPUslT2kbA.jpg"
+];
+
+$filme3 = [
+  "titulo" => "Peaky Blinders",
+  "nota" => 9.8,
+  "sinopse" => "Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Mé faiz elementum girarzis, nisi eros vermeio.",
+  "poster" => "https://www.themoviedb.org/t/p/original/i0uajcHH9yogXMfDHpOXexIukG9.jpg"
+];
+
+$filmes = [$filme1, $filme2, $filme3];
+
 ?>
 
 <body>
@@ -32,39 +48,26 @@ $filme1 = [
   </nav>
 
   <div class="row">
-    <div class="col s3">
-      <div class="card hoverable">
-        <div class="card-image">
-          <img src="<?= $filme1['poster'] ?>">
 
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
-        </div>
-        <div class="card-content">
-          <p class="valign-wrapper">
-            <i class="material-icons amber-text">star</i> <?= $filme1['nota'] ?>
-          </p>
-          <span class="card-title"><?= $filme1['titulo'] ?></span>
-          <p><?= $filme1['sinopse'] ?></p>
+    <?php foreach($filmes as $filme) : ?>
+
+      <div class="col s3">
+        <div class="card hoverable">
+          <div class="card-image">
+            <img src="<?= $filme['poster'] ?>">
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+          </div>
+          <div class="card-content">
+            <p class="valign-wrapper">
+              <i class="material-icons amber-text">star</i> <?= $filme['nota'] ?>
+            </p>
+            <span class="card-title"><?= $filme['titulo'] ?></span>
+            <p><?= $filme['sinopse'] ?></p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="col s3">
-      <div class="card hoverable">
-        <div class="card-image">
-          <img src="https://www.themoviedb.org/t/p/original/qhcwrnnCnN8NE1N6XXKHFmveJR9.jpg">
-
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
-        </div>
-        <div class="card-content">
-          <p class="valign-wrapper">
-            <i class="material-icons amber-text">star</i>10
-          </p>
-          <span class="card-title">Umbrella Academy</span>
-          <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
-        </div>
-      </div>
-    </div>
+    <?php endforeach ?>
 
   </div>
 
